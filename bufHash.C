7c5663da5af19660a7f237c1b9a46347be30fd3f
k-oku <kmoku@wisc.edu>
2022-11-03 16:35:51 -0500

    update bufhash

--- bufHash.C
+++ bufHash.C
@@ -12,7 +12,7 @@
 
 int BufHashTbl::hash(const File* file, const int pageNo)
 {
-  int tmp, value;
+  long tmp, value;
   tmp = (long)file;  // cast of pointer to the file object to an integer
   value = (tmp + pageNo) % HTSIZE;
   return value;
